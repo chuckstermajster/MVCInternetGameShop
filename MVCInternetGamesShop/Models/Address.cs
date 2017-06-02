eef9f699fa0187eb4ca0df5abe9aaa13a2a34ccd
chuckstermajster <chuckstermajster@gmail.com>
2017-06-02 13:09:06 +0200

    enable migrations

---
+++ MVCInternetGamesShop/Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GamesShop.Models
+{
+    public class Address
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane")]
+        public int StreetNumber { get; set; }
+
+        public int HouseNumber { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane")]
+        public string PostCode { get; set; }
+
+        
+
+    }
+}
---
+++ MVCInternetGamesShop/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamesShop.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public Address Address { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        
+
+    }
+}
